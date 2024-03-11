98a1d4a51e8e92e8b8600cced8a542085f1ddfd1
Vittoamedeo <157356895+Vittorioits@users.noreply.github.com>
2024-03-11 12:28:23 +0100

    Add files via upload

---
+++ its_update.js
@@ -0,0 +1,24 @@
+async function wikifetch() {
+    const response = await fetch('https://raw.githubusercontent.com/dariusk/corpora/master/data/technology/programming_languages_popular.json');
+    const json = await response.json();
+    let elementi = document.getElementsByClassName("item_text");
+    
+
+    for (let y=0; y < elementi.length; y++) {
+        elementi[y].innerHTML= json.programming_languages_popular[y];
+    }
+
+    
+    let img= document.getElementsByClassName("eltdf-normal-logo");
+    console.log(img.length);
+
+    img[0].src="https://static.wixstatic.com/media/44764b_8742a1151cb34cce9adf250e6bd49490~mv2.png/v1/fill/w_2048,h_2048,al_c/w3schools-2048.png";
+    
+}
+   
+wikifetch();
+    
+    
+
+   
+    
